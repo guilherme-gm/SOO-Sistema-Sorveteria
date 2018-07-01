f379d01912ecea72a1704ae592907ba06962b342
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-07-01 14:58:42 -0300

    Página de venda

--- Sorveteria/WebContent/js/venda.js
+++ Sorveteria/WebContent/js/venda.js
@@ -1,7 +1,160 @@
-$("#btn-buscar").on("click", function() {
-	
+/**
+ * Atrasa a execução de um método para quando o jQuery estiver disponível
+ * 
+ * @param method
+ * @returns
+ */
+function defer(method) {
+	if (window.jQuery) {
+		method();
+	} else {
+		setTimeout(function() {
+			defer(method)
+		}, 50);
+	}
+}
+
+/**
+ * Adiciona uma coluna na linha row, com o valor data, podendo ser oculta
+ * 
+ * @param row
+ * @param data
+ * @param visible
+ * @returns
+ */
+function makeCell(row, data, visible = true) {
+	let cel = row.insertCell();
+	cel.innerHTML = data;
+	if (visible == false) {
+		cel.style.display = "none";
+	}
+}
+
+/**
+ * Carregamento das funções que dependem do jQuery
+ * 
+ * @returns
+ */
+defer(function() {
+
+	/**
+	 * Pressionar enter no campo de busca
+	 */
+	$("#busca-text").on("keydown", function(event) {
+		if (event.keyCode == 13) {
+			buscarProduto();
+		}
+	});
+
+	/**
+	 * Botão de Busca
+	 */
+	$("#btn-buscar").on("click", buscarProduto);
+
 });
 
-function adicionarItemVenda(id) {
+/**
+ * Busca o termo na caixa de busca
+ * 
+ * @returns
+ */
+function buscarProduto() {
+	let termo = $("#busca-text").val();
+
+	$.get("BuscaProdutoPorNome", {
+		nome : termo
+	}).done(preencheResultado).fail(function() {
+		alert("Ocorreu um erro ao realizar a busca. Tente novamente.");
+	});
+}
+
+/**
+ * Preenche a área de resultado da busca
+ * 
+ * @param data
+ * @returns
+ */
+function preencheResultado(data) {
+	let old_tbody = document.getElementById("table-busca");
+	let new_tbody = document.createElement('tbody');
+	new_tbody.id = "table-busca";
 	
+	json = JSON.parse(data);
+	json.forEach(function(prod) {
+		let row = new_tbody.insertRow();
+		row.id = "busca-" + prod.codigo; 
+		makeCell(row, prod.nome);
+		makeCell(row, prod.categoria);
+		makeCell(row, prod.quantidadeEstoque);
+		makeCell(row, prod.precoVenda);
+		makeCell(row, '<button class="btn btn-success" onclick="adicionarItemVenda(' + prod.codigo + ')"><i class="mdi mdi-plus"></i></button>');
+	});
+	
+	old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+}
+
+/**
+ * Adiciona um item na venda
+ * 
+ * @param id
+ * @returns
+ */
+function adicionarItemVenda(id) {
+	let info = $("#busca-"+id);
+	
+	if (info.length <= 0 || $("#item-"+id).length > 0) {
+		return;
+	}
+	
+	let tbody = document.getElementById("table-venda");
+	let row = tbody.insertRow();
+	row.id = "item-"+id;
+	makeCell(row, info[0].cells[0].innerText); // nome
+	makeCell(row, info[0].cells[1].innerText); // categoria
+	makeCell(row, info[0].cells[2].innerText); // Estoque
+	makeCell(row, info[0].cells[3].innerText); // Preço
+	makeCell(row, '<input type="number" name="quantidade" value="1" min="1" onchange="quantidadeMudou()" max="'+info[0].cells[2].innerText+'" />'); // Quantidade
+	makeCell(row, '<button class="btn btn-danger" onclick="removerItemVenda(' + id + ')"><i class="mdi mdi-minus"></i></button>');
+	makeCell(row, '<input type="hidden" name="produto" value="'+id+'" />', false); // ID
+																					// (hidden)
+	
+	quantidadeMudou();
+}
+
+/**
+ * Quantidade de um item mudou
+ * 
+ * @param id
+ * @returns
+ */
+function quantidadeMudou() {
+	let tbody = $("#table-venda tr");
+	let total = 0.0;
+	tbody.each(function (i, row) {
+		let r = $(row),
+        	qtd = parseInt(r.find('input[name*="quantidade"]').val()),
+        	preco = row.cells[3].innerText;
+		
+		total += preco * qtd;
+	});
+	
+	$("#total-venda").text(total);
+}
+
+/**
+ * Remove um item dos itens da venda
+ * 
+ * @param id
+ * @returns
+ */
+function removerItemVenda(id) {
+	let item = $("#item-"+id);
+	
+	if (item.length <= 0) {
+		return;
+	}
+	
+	quantidadeMudou();
+	
+	item.remove();
 }
--- Sorveteria/WebContent/pages/venda/registrar.jsp
+++ Sorveteria/WebContent/pages/venda/registrar.jsp
@@ -1,43 +1,38 @@
 <h2>Registrar Venda</h2>
-<form method="POST" action="#">
-	<div class="card">
-		<div class="card-body">
-			<h3>Busca</h3>
-			<div class="form-row">
-				<div class="col">
-					<input type="text" class="form-control" placeholder="Busca">
-				</div>
-				<div class="col">
-					<button id="btn-buscar" class="btn btn-primary">Buscar</button>
-				</div>
+<div class="card">
+	<div class="card-body">
+		<h3>Busca</h3>
+		<div class="form-row">
+			<div class="col">
+				<input id="busca-text" type="text" class="form-control"
+					placeholder="Busca">
 			</div>
-			<div class="form-group">
-				<table class="table table-hover">
-					<thead>
-						<tr>
-							<th>Nome</th>
-							<th>Categoria</th>
-							<th>Estoque</th>
-							<th>Preço</th>
-							<th>Ações</th>
-						</tr>
-					</thead>
-					<tbody class="items-busca">
-						<tr>
-							<td>Nap</td>
-							<td>MAssa</td>
-							<td>10</td>
-							<td>R$10</td>
-							<td>+</td>
-						</tr>
-					</tbody>
-
-				</table>
+			<div class="col">
+				<button id="btn-buscar" type="button" class="btn btn-primary">Buscar</button>
 			</div>
 		</div>
-	</div>
+		<div class="form-group">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>Categoria</th>
+						<th>Estoque</th>
+						<th>Preço</th>
+						<th>Ações</th>
+					</tr>
+				</thead>
+				<tbody id="table-busca">
+				</tbody>
 
-	<br /> <br />
+			</table>
+		</div>
+	</div>
+</div>
+
+<br />
+<br />
+<form method="POST" action="#">
 
 	<div class="card">
 		<div class="card-body">
@@ -48,32 +43,27 @@
 						<tr>
 							<th>Nome</th>
 							<th>Categoria</th>
-							<th>Quantidade</th>
+							<th>Em estoque</th>
 							<th>Preço</th>
+							<th>Quantidade</th>
 							<th>Ações</th>
 						</tr>
 					</thead>
-					<tbody class="items-venda">
-						<tr>
-							<td>Nap</td>
-							<td>MAssa</td>
-							<td>10</td>
-							<td>R$10</td>
-							<td>-</td>
-						</tr>
+					<tbody id="table-venda">
 					</tbody>
 
 				</table>
 			</div>
 
-			<div class="form-group float-right">
-				<label>Total:</label> R$<span id="total-venda">0,00</span>
+			<div class="form-group">
+				<div class="float-right">
+					<label>Total:</label> R$<span id="total-venda">0,00</span>
+				</div>
+				<div class="float-none"></div>
 			</div>
-			<div class="float-none"></div>
 		</div>
 	</div>
 
-	<button type="reset" class="btn btn-primary">Limpar</button>
 	<button type="submit" class="btn btn-primary">Confirmar</button>
 </form>
 
