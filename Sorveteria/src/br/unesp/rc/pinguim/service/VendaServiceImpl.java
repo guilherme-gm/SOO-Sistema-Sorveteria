e25622358c474d17af00e60b22e66ae1160bd6c3
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-07-02 10:06:51 -0300

    Removido import não usado

--- Sorveteria/src/br/unesp/rc/pinguim/service/VendaServiceImpl.java
+++ Sorveteria/src/br/unesp/rc/pinguim/service/VendaServiceImpl.java
@@ -7,7 +7,6 @@ import br.unesp.rc.pinguim.dao.VendaDAO;
 import br.unesp.rc.pinguim.models.ItemVenda;
 import br.unesp.rc.pinguim.models.Produto;
 import br.unesp.rc.pinguim.models.Venda;
-import br.unesp.rc.pinguim.utils.FabricaConexao;
 
 /**
  * Service com ações relacionadas a uma Venda
